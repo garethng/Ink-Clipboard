0b38368ed1829243c2e892e857c01b035387f657
Gareth Ng <njwqz77@gmail.com>
2023-12-28 23:50:26 +0800

    :lipstick: Update ui style to beautify

--- src/app/MainPage/page.tsx
+++ src/app/MainPage/page.tsx
@@ -15,14 +15,18 @@ const MainPage: React.FC = () => {
     const [isLoading, setIsLoading] = useState(false)
     const [data, setData] = useState < { key: string; label: string; children: {key:string, content: string}[] }[] | null>(null)
     return (
+        
         <RefreshLoadingContext.Provider value={{ isLoading, setLoading: setIsLoading, data, setData: setData }}>
             <MyTextarea></MyTextarea>
             <RefreshButton ></RefreshButton>
+            <div className="sm:py-[10vh] flex flex-col items-center justify-center max-w-[400px] w-full">
+                <MyCollapse />
+            </div>
             
-            <MyCollapse />
             
             
         </RefreshLoadingContext.Provider>
+        
     )
  }
 
