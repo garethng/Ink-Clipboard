575b2b2b8cbcd0a2a65b5d123c994fa76a384360
Gareth Ng <njwqz77@gmail.com>
2023-12-28 21:16:35 +0800

    :bug: Fix type error

--- src/app/MainPage/page.tsx
+++ src/app/MainPage/page.tsx
@@ -13,7 +13,7 @@ import RefreshLoadingContext from '@/utils/refreshContext';
 
 const MainPage: React.FC = () => {
     const [isLoading, setIsLoading] = useState(false)
-    const [data, setData] = useState < { key: string; label: string; children: {key:number, content: string}[] }[] | null>(null)
+    const [data, setData] = useState < { key: string; label: string; children: {key:string, content: string}[] }[] | null>(null)
     return (
         <RefreshLoadingContext.Provider value={{ isLoading, setLoading: setIsLoading, data, setData: setData }}>
             <MyTextarea></MyTextarea>
