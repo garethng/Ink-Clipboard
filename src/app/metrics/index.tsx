3120748b3cf67bfc110d2e6bb16c9c4ee95d55b1
Qingzhe Wu <qingzhe_wu@trendmicro.com>
2024-01-26 17:30:42 +0800

    📈 add GA and microsoft clarity

---
+++ src/app/metrics/index.tsx
@@ -0,0 +1,11 @@
+import GoogleAnalytics from "./GoogleAnalytics"
+import MicrosoftClarity from "./MicrosoftClarity"
+
+const Metrics = () => (
+    <>
+        <GoogleAnalytics />
+        <MicrosoftClarity />
+    </>
+)
+
+export default Metrics
