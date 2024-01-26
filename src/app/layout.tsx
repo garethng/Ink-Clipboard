f243c859ef261f132a66b64a5ff9140eb5b2f611
Qingzhe Wu <qingzhe_wu@trendmicro.com>
2024-01-26 17:30:42 +0800

    📈 add GA and microsoft clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 
 import { ClerkProvider } from "@clerk/nextjs";
+import Metrics from './metrics'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,6 +21,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>{children}</body>
+        <Metrics />
       </html>
     </ClerkProvider>
   )
